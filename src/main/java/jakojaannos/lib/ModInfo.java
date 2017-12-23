3c05589e48ea63bcf9e6241dd9fd683bfed06a67
Kailari <ilari97@hotmail.com>
2017-12-23 19:21:31 +0200

    Made build automatically update the version metadata

--- src/main/java/jakojaannos/lib/ModInfo.java
+++ src/main/java/jakojaannos/lib/ModInfo.java
@@ -5,5 +5,5 @@ package jakojaannos.lib;
 class ModInfo {
     public static final String MODID = "jakojaannos-lib";
     public static final String NAME = "Team Jakojäännös Lib";
-    public static final String VERSION = "0.0.0";
+    public static final String VERSION = "__VERSION__";
 }
