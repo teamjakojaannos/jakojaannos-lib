72496d85d389ce8cb8e83356ea246e350e94f4b2
Kailari <ilari97@hotmail.com>
2018-01-02 20:54:42 +0200

    Removed the config stuff and changed the way block lookup is generated. Goal is to change generation parameters to be driven by generator options instead of config

--- src/api/java/jakojaannos/api/world/AdvancedBiomeBase.java
+++ src/api/java/jakojaannos/api/world/AdvancedBiomeBase.java
@@ -1,7 +1,5 @@
 package jakojaannos.api.world;
 
-import com.google.common.collect.ImmutableList;
-import jakojaannos.api.helpers.BlockHelper;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
@@ -9,36 +7,23 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.ChunkPrimer;
-import net.minecraftforge.common.config.Config.Comment;
 
-import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
-
-// TODO: Apply noiseVal to fillerDepth etc.
 
 /**
  * Adds some convenience features for allowing a bit more customized biomes.
  */
-public class AdvancedBiomeBase extends Biome {
+public abstract class AdvancedBiomeBase extends Biome {
+    private static final IBlockState[] LOOKUP = new IBlockState[256];
+
     private int seaLevelOverride;
     private int bedrockDepth;
-    private int fillerDepth;
 
     private float seaLevelFuzzScale;
     private float seaLevelFuzzOffset;
 
     private IBlockState oceanBlock;
     private IBlockState stoneBlock;
-
-    private final @Nullable List<BlockLayer> overwaterLayers;
-    private final @Nullable List<BlockLayer> underwaterLayers;
-
-    private IBlockState[] blockStateLookup;
-    private IBlockState[] underwaterBlockStateLookup;
 
 
     /**
@@ -101,50 +86,6 @@ public class AdvancedBiomeBase extends Biome {
         return this;
     }
 
-    /**
-     * Gets the filler block
-     */
-    public IBlockState getFillerBlock() {
-        return fillerBlock;
-    }
-
-    /**
-     * Sets the filler block
-     */
-    public AdvancedBiomeBase setFillerBlock(IBlockState fillerBlock) {
-        this.fillerBlock = fillerBlock;
-        return this;
-    }
-
-    /**
-     * Gets the top block
-     */
-    public IBlockState getTopBlock() {
-        return topBlock;
-    }
-
-    /**
-     * Sets the top block
-     */
-    public AdvancedBiomeBase setTopBlock(IBlockState topBlock) {
-        this.topBlock = topBlock;
-        return this;
-    }
-
-    /**
-     * Gets the number of filler block layers to generate
-     */
-    public int getFillerDepth() {
-        return fillerDepth;
-    }
-
-    /**
-     * Sets the number of filler block layers to generate
-     */
-    public AdvancedBiomeBase setFillerDepth(int fillerDepth) {
-        this.fillerDepth = fillerDepth;
-        return this;
-    }
 
     /**
      * Gets the fuzz-scale when transitioning from overwater layers to underwater ones
@@ -176,42 +117,20 @@ public class AdvancedBiomeBase extends Biome {
         return seaLevelOverride < 0 ? world.getSeaLevel() : seaLevelOverride;
     }
 
-    protected AdvancedBiomeBase(BiomeProperties properties, Config config) {
+
+    public abstract BlockLayer[] getLayers(boolean underwater);
+
+
+    protected AdvancedBiomeBase(BiomeProperties properties) {
         super(properties);
-
-        this.overwaterLayers = Arrays.stream(config.layers).map(BlockLayer::new).collect(Collectors.toList());
-        this.underwaterLayers = Arrays.stream(config.underwaterLayers).map(BlockLayer::new).collect(Collectors.toList());
-
-        this.fillerDepth = config.fallBackFillerDepth;
-        this.topBlock = BlockHelper.stringToBlockstateWithFallback(Blocks.GRASS.getDefaultState(), config.fallbackTopBlock);
-        this.fillerBlock = BlockHelper.stringToBlockstateWithFallback(Blocks.DIRT.getDefaultState(), config.fallbackFillerBlock);
-
-        this.stoneBlock = BlockHelper.stringToBlockstateWithFallback(Blocks.STONE.getDefaultState(), config.stoneBlock);
-        this.oceanBlock = BlockHelper.stringToBlockstateWithFallback(Blocks.WATER.getDefaultState(), config.oceanBlock);
-
-        this.bedrockDepth = config.bedrockDepth;
-        this.seaLevelFuzzScale = config.seaLevelFuzzScale;
-        this.seaLevelFuzzOffset = config.seaLevelFuzzOffset;
-        this.seaLevelOverride = config.seaLevelOverride;
-
-        blockStateLookup = null;
-    }
-
-    protected AdvancedBiomeBase(BiomeProperties properties, @Nullable List<BlockLayer> layers, @Nullable List<BlockLayer> underwaterLayers) {
-        super(properties);
-        this.overwaterLayers = layers == null ? null : ImmutableList.copyOf(layers);
-        this.underwaterLayers = underwaterLayers == null ? null : ImmutableList.copyOf(underwaterLayers);
 
         this.oceanBlock = WATER;
         this.stoneBlock = STONE;
-        this.fillerDepth = 5;
         this.bedrockDepth = 5;
         this.seaLevelOverride = -1;
 
-        this.seaLevelFuzzOffset = 3.0f;
-        this.seaLevelFuzzScale = 3.0f;
-
-        blockStateLookup = null;
+        this.seaLevelFuzzOffset = 0.0f;
+        this.seaLevelFuzzScale = 0.0f;
     }
 
     @Override
@@ -226,7 +145,6 @@ public class AdvancedBiomeBase extends Biome {
         final int seaLevel = getSeaLevel(world);
         final int fuzzySeaLevel = MathHelper.floor((seaLevel + getSeaLevelFuzzOffset()) + (noiseVal * getSeaLevelFuzzScale()));
         boolean hitSolid = false;
-        boolean underwater = false;
         int solidDepth = 0;
 
         // Replace blocks
@@ -237,131 +155,59 @@ public class AdvancedBiomeBase extends Biome {
             // Keep replacing water blocks with air or water override block until we hit solid
             if (!hitSolid) {
                 if (state.getMaterial() == Material.WATER) {
-                    replaceWater(world, rand, primer, seaLevel, x, y, z, noiseVal);
+                    final IBlockState newState = y > seaLevel ? Blocks.AIR.getDefaultState() : oceanBlock;
+                    primer.setBlockState(x, y, z, newState);
                 } else if (state.getMaterial() != Material.AIR) {
                     hitSolid = true;
-                    underwater = y <= fuzzySeaLevel;
+
+                    // Generate temporary lookup table to speed up the block selection
+                    generateLookup(rand, y, fuzzySeaLevel, globalX, globalZ, noiseVal, LOOKUP);
                 }
             }
 
             // Replace first solid with top block and the rest with filler blocks.
             if (hitSolid) {
-                replaceSolid(world, rand, primer, seaLevel, x, y, z, noiseVal, solidDepth++, underwater);
+                primer.setBlockState(x, y, z, LOOKUP[solidDepth++]);
             }
         }
     }
+
 
     /**
-     * Returns true if placed block was ocean block
+     * Generates lookup table for blockstates in a single column. DO NOT CREATE NEW ARRAY ON EACH CALL, use the array
+     * provided in "lookup" parameter as it is recycled.
+     *
+     * @param solidY        y-coordinate of the first solid layer
+     * @param fuzzySeaLevel sea level with fuzz applied
+     * @param x             global x-coordinate of the column
+     * @param z             global z-coordinate of the column
      */
-    protected void replaceWater(World world, Random random, ChunkPrimer primer, int seaLevel, int x, int y, int z, double noiseVal) {
-        final IBlockState newState = y > seaLevel ? Blocks.AIR.getDefaultState() : oceanBlock;
-        primer.setBlockState(x, y, z, newState);
-    }
-
-    protected void replaceSolid(World world, Random random, ChunkPrimer primer, int seaLevel, int x, int y, int z, double noiseVal, int solidDepth, boolean underwater) {
+    protected void generateLookup(Random random, int solidY, int fuzzySeaLevel, int x, int z, double noiseVal, IBlockState[] lookup) {
+        final boolean underwater = solidY <= fuzzySeaLevel;
+        final BlockLayer[] layers = getLayers(underwater);
         final int fuzzyBedrockDepth = bedrockDepth; // TODO: apply fuzz
 
-        final IBlockState[] lookup = getBlockStateLookup(underwater);
-
-        // Randomly replace blocks with bedrock starting at bedrock depth with frequency of (1 / bedrockDepth) and
-        // increasing to frequency of 100% at bottommost layer. Topmost solid layer can never be bedrock.
-        if (solidDepth == 0) {
-            primer.setBlockState(x, y, z, lookup[solidDepth]);
-        } else if (y <= random.nextInt(fuzzyBedrockDepth)) {
-            primer.setBlockState(x, y, z, Blocks.BEDROCK.getDefaultState());
-        } else {
-            primer.setBlockState(x, y, z, lookup[solidDepth]);
-        }
-    }
-
-    protected IBlockState[] getBlockStateLookup(boolean underwater) {
-        if (underwater) {
-            if (underwaterBlockStateLookup == null) {
-                underwaterBlockStateLookup = generateLookup(underwaterLayers, GRAVEL, STONE);
-            }
-
-            return underwaterBlockStateLookup;
-        } else {
-            if (blockStateLookup == null) {
-                blockStateLookup = generateLookup(overwaterLayers, topBlock, fillerBlock);
-            }
-
-            return blockStateLookup;
-        }
-    }
-
-    private IBlockState[] generateLookup(@Nullable List<BlockLayer> layers, IBlockState top, IBlockState filler) {
-        IBlockState[] lookup = new IBlockState[256];
-
-        if (layers == null) {
-            lookup[0] = top;
-            for (int y = 1; y < 256; y++) {
-                if (y <= fillerDepth) {
-                    lookup[y] = filler;
-                } else {
-                    lookup[y] = stoneBlock;
-                }
-            }
-        } else {
-            int y = 0;
-            for (BlockLayer layer : layers) {
-                int i = 0;
-                while (y < 256 && i < layer.getDepth()) {
+        int y = 0;
+        for (BlockLayer layer : layers) {
+            int i = 0;
+            while (y < 256 && i < layer.getDepth()) {
+                if (y == 0) {
                     lookup[y] = layer.getBlock();
-                    i++;
-                    y++;
+                } else if (y > random.nextInt(fuzzyBedrockDepth - solidY)) {
+                    lookup[y] = Blocks.BEDROCK.getDefaultState();
+                } else {
+                    lookup[y] = layer.getBlock();
                 }
-            }
 
-            while (y < 256) {
-                lookup[y] = stoneBlock;
+                i++;
                 y++;
             }
         }
 
-        return lookup;
-    }
-
-
-    /**
-     * Allows constructing advanced biomes from config files
-     */
-    public static class Config {
-        @Comment("Overrides the world sea y-level for this biome. Set to negative value for no override")
-        public int seaLevelOverride = -1;
-
-        @Comment("Y-level offset for switching between underwater and normal layers when near sea level")
-        public float seaLevelFuzzOffset = 0.0f;
-
-        @Comment("Y-level fuzz scale for switching between underwater and normal layers when near sea level")
-        public float seaLevelFuzzScale = 1.0f;
-
-        @Comment("Number of bedrock layers to generate.")
-        public int bedrockDepth = 5;
-
-
-        @Comment("After layers are generated, all remaining solid blocks below them are set to this")
-        public String stoneBlock = "minecraft:stone";
-
-        @Comment("Replaces ocean water with this block")
-        public String oceanBlock = "minecraft:water";
-
-
-        @Comment("If layers definition is invalid, generate filler layer with this depth")
-        public int fallBackFillerDepth = 5;
-
-        @Comment("If layers definition is invalid, use this block as fallback top layer")
-        public String fallbackTopBlock = "minecraft:grass";
-
-        @Comment("If layers definition is invalid, use this block as fallback filler layer")
-        public String fallbackFillerBlock = "minecraft:dirt";
-
-
-        @Comment("Layers to generate. These are generated in order they are defined, relative to the first solid block counting from sky towards bedrock")
-        public String[] layers = {};
-
-        @Comment("Layers to use when below sea level")
-        public String[] underwaterLayers = {};
+        // Fill the rest of blocks with stone blocks
+        while (y < 256) {
+            lookup[y] = stoneBlock;
+            y++;
+        }
     }
 }
